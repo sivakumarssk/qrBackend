2626acc9b3c489a067d25d295d7bb30fca093ce4
sivakumarssk <sivakumarslks@gmail.com>
2025-02-12 02:27:01 +0530

    razorpay added

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router=express.Router()
+const Razorpay = require("razorpay");
 
 const userControler=require('../controlers/userLoginControler');
 const { uploadqrFilesFiles } = require('../controlers/qrFilesControler');
@@ -38,5 +39,29 @@ router.post('/addPrice', updatePrice);
 
 router.get('/referals',getReferals);
 router.post('/addreferals', createReferal);
+
+const razorpay = new Razorpay({
+    key_id: "rzp_live_HJLLQQPlyQFOGr",
+    key_secret: "cm2v1OSggPZ5vVHX5rl3jrq4",
+  });
+  
+  // Create order endpoint
+  router.post("/create-order", async (req, res) => {
+    try {
+      const { amount, currency } = req.body;
+  
+      const options = {
+        amount: amount * 100, // Convert to paise
+        currency,
+        receipt: `receipt_${Math.random().toString(36).substring(2, 15)}`,
+      };
+  
+      const order = await razorpay.orders.create(options);
+      res.json({ orderId: order.id, amount: order.amount });
+    } catch (error) {
+      res.status(500).json({ error: "Failed to create order", details: error });
+    }
+  });
+  
 
 module.exports=router
